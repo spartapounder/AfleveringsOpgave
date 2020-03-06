2ae9b3e8d89ded9008179d4df8e435ab21a17e75
Alexander Georg Anestopoulos Pedersen <alexander.g.a.pedersen@live.dk>
2020-03-06 10:58:37 +0100

    Hej

---
+++ AfleveringsOpgave/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+
+
+namespace AfleveringsOpgave
+{
+    
+    public partial class MainWindow : Window
+    {
+        List<User> users = new List<User>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void OpenFileDialog(object sender, RoutedEventArgs e)
+        {
+
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.DefaultExt = ".txt";
+            dlg.Filter = "txt Files (*.txt)|*.txt";
+
+            if (dlg.ShowDialog() == true)
+            {
+                string filename = dlg.FileName;
+                
+                using(StreamReader SR = new StreamReader(filename))
+                {
+                    string line;
+                    while ((line = SR.ReadLine()) != null)
+                    {
+                        string[] data = line.Split(';');
+                        users.Add(new User(int.Parse(data[0]), data[1], int.Parse(data[2]), int.Parse(data[3])));
+                    }
+                }
+
+                PersonBox.ItemsSource = users;
+                bigGrid.DataContext = users;
+
+                lines.Text = "Users: " + users.Count().ToString() + ",";
+                time.Text = "Time loaded: " + DateTime.Now.ToString("HH:mm:ss");
+            }
+        }
+    }
+}
---
+++ AfleveringsOpgave/User.cs
@@ -0,0 +1,23 @@
+﻿
+namespace AfleveringsOpgave
+{
+    class User
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int age { get; set; }
+        public int score { get; set; }
+
+        public User(int _id, string _name, int _age, int _score)
+        {
+            id = _id;
+            name = _name;
+            age = _age;
+            score = _score;
+        }
+        public override string ToString()
+        {
+            return (id.ToString() + " - " + name);
+        }
+    }
+}
